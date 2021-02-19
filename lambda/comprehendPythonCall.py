97e25d230af44ceee6c6f7305d6e0b7871b7f16e
caibobob <bob.cai@qq.com>
2021-02-19 11:38:06 +0800

    Update comprehendPythonCall.py
    update with lambda version

--- lambda/comprehendPythonCall.py
+++ lambda/comprehendPythonCall.py
@@ -1,37 +1,30 @@
 import json
 import urllib.parse
 import os
-import boto4
+import boto3
 
-s3 = boto4.client('s3')
-comprehend = boto4.client('comprehend',region_name='us-west-2')
+s3 = boto3.client('s3')
+comprehend = boto3.client('comprehend',region_name='ap-northeast-1')
 
 def lambda_handler(event, context):
-    # print(dir(comprehend))
+    print(f"ecent:{event}")
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     print(key)
-    s3uri = 's3://'+bucket+'/'+key.split("/")[0]+'/'
+    s3uri = 's3://'+bucket+'/'+key
     directoryName = key.split("/")[0]
-    print(s3uri)
-    try:
-        response = comprehend.start_document_classification_job(
-            # JobName='test',
-            DocumentClassifierArn='arn:aws:comprehend:us-west-2:account-id:document-classifier/SAIHE',
-            InputDataConfig={
-                'S3Uri': s3uri,
-                'InputFormat': 'ONE_DOC_PER_LINE'
-            },
-            OutputDataConfig={
-                'S3Uri': 's3://mailsupport-2/'+directoryName+'/'
-            },
-            DataAccessRoleArn='arn:aws:iam::account-id:role/service-role/AmazonComprehendServiceRole-comprehens3'
-        )
-        print(response)
-        
-        
-        
-        print("**************** Job finish ****************")        
-    except Exception as e:
-        print('**************** process error **************')
-        print(e)
+    print(f"s3uri:{s3uri}")
+    # TODO implement
+    response = comprehend.start_document_classification_job(
+    JobName='ecommerce',
+    DocumentClassifierArn='arn:aws:comprehend:ap-northeast-1:accountid:document-classifier/ecommerce',
+    InputDataConfig={
+        'S3Uri': s3uri,
+        'InputFormat': 'ONE_DOC_PER_LINE'
+    },
+    OutputDataConfig={
+        'S3Uri': 's3://aws-caibc-comprehen-training-tokyo/result/',
+    },
+    DataAccessRoleArn='arn:aws:iam::accountid:role/service-role/AmazonComprehendServiceRole-comprehens3',
+    )
+    print(f"Comprehend response:{response}")
